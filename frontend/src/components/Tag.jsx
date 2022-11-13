597c7d95cfaaa52890337257212f79ff581fc7d7
Anthony Gorski <[anthony.gorski@wildcodeschool.com](mailto:anthony.gorski@wildcodeschool.com)>
2022-11-13 20:45:31 +0100

    debug(tag): change tag pills

--- frontend/src/components/Tag.jsx
+++ frontend/src/components/Tag.jsx
@@ -1,10 +1,18 @@
 import PropTypes from "prop-types";
 
 function Tag({ name }) {
+  const tags = name.split(",");
+
   return (
-    <span className="inline-flex items-center rounded-full bg-amber-600 px-3 py-.5 px-5 text-sm font-medium text-pink-50 mx-1">
-      {name}
-    </span>
+    <>
+      {tags.map((tag) => {
+        return (
+          <span className="inline-flex items-center rounded-full bg-amber-600 py-.5 px-5 text-sm font-medium text-pink-50 mx-1">
+            {tag}
+          </span>
+        );
+      })}
+    </>
   );
 }
 
